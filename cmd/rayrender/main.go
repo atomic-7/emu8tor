1a9da7868b173b8b82862cc23290a52281263f45
atomic <atomic@myps.de>
2024-04-07 18:08:05 +0200

    Grid render

--- cmd/rayrender/main.go
+++ cmd/rayrender/main.go
@@ -9,7 +9,16 @@ import (
 
 func colorGenerator(bufChan chan []byte) {
 	localBuf := make([]byte, 64 * 32)
-	for count := 0; count < 200; count++ {
+	for count := 0; count < 32; count++ {
+
+		if count % 2 == 0 {
+			for idx := 0; idx < 64; idx++ {
+				if idx % 2 == 0 {
+					localBuf[count * 64 + idx] = byte(100)
+				}
+			}
+		}
+
 		localBuf[0] = byte(100 + 20 * (count % 3))
 		localBuf[1] = byte(100 + 10 * (count % 5))
 		localBuf[2] = byte(100 + 30 * (count % 7))
@@ -23,17 +32,16 @@ func colorGenerator(bufChan chan []byte) {
 func main() {
 	fmt.Println("RAYGRAPHICS")
 
-	render := raygraphics.NewRaylibRender(800, 450)
+	render := raygraphics.NewRaylibRender(64, 32)
 	bufChan := make(chan []byte)
 	lastBuf := make([]byte, 64 * 32)
 
-	rl.InitWindow(800, 450, "RayRender tests")
+	rl.InitWindow(1024, 512, "RayRender tests")
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
 	go colorGenerator(bufChan)
 
 	for !rl.WindowShouldClose() {
-	
 		select {
 		case buf := <- bufChan:
 			copy(lastBuf, buf)	
--- internal/raylibgraphics/graphics.go
+++ internal/raylibgraphics/graphics.go
@@ -7,8 +7,8 @@ import (
 // Implements Drawable
 
 type RayRender struct {
-	Width int
-	Height int
+	Width   int
+	Height  int
 	rayChan chan []byte
 }
 
@@ -24,12 +24,20 @@ func (rr *RayRender) Draw(dbuf []byte) {
 	rr.rayChan <- dbuf
 }
 func (rr *RayRender) DrawBuf(dbuf []byte) {
-	color := rl.NewColor(dbuf[0],dbuf[1],dbuf[2], 255)
+	color := rl.NewColor(dbuf[0], dbuf[1], dbuf[2], 255)
+	pxlLen := int32(rl.GetScreenWidth() / rr.Height)
+
 	rl.BeginDrawing()
 	rl.ClearBackground(color)
-	rl.DrawText("Raylib Woohoo!", 190, 200, 20, rl.LightGray)
+	var x,y int32
+	for y = 0; y < int32(rr.Height); y++ {
+		for x = 0; x < int32(rr.Width); x++ {
+			if dbuf[y * int32(rr.Width) + x] > 0 {
+				rl.DrawRectangle(x*pxlLen, y*pxlLen, pxlLen, pxlLen, rl.Black)
+			}
+		}
+	}
 	rl.EndDrawing()
-
 }
 
 func NewRaylibRender(w int, h int) *RayRender {
@@ -40,6 +48,7 @@ func NewRaylibRender(w int, h int) *RayRender {
 
 	return &rr
 }
+
 /*
 	GetScreenWidth()
 	void EnableEventWaiting(void)
