59ebb426028e9a9c2983609cfb1dad75c340abdd
atomic <atomic@myps.de>
2024-04-24 02:28:33 +0200

    Tracking chip architecture, added readable string printing for chip

--- internal/chip8/chip.go
+++ internal/chip8/chip.go
@@ -2,6 +2,7 @@ package chip8
 
 import (
 	"errors"
+	"fmt"
 )
 
 // Chip8 should represent the memory as seen by an original chip8 game
@@ -14,11 +15,18 @@ type Chip8 struct {
 	I         int16
 	Width     int16
 	Height    int16
+	Architecture int
 }
+
+const (
+	CHIP8 = iota
+	CHIP48
+	SCHIP
+)
 
 // both pc and index counter I can only adress 12 bits = 4096 addresses
 
-func NewChip8() *Chip8 {
+func NewChip8(instructionset int) *Chip8 {
 	var chip Chip8
 	chip.Width = 64
 	chip.Height = 32
@@ -27,10 +35,11 @@ func NewChip8() *Chip8 {
 	chip.Registers = make([]int8, 16, 16)
 	chip.Display = make([]byte, chip.Width*chip.Height, chip.Width*chip.Height)
 	chip.Stack = &Stack{
-		sp:0,
+		sp: 0,
 	}
 	chip.PC = 0
 	chip.I = 0x020
+	chip.Architecture = instructionset
 	return &chip
 }
 
@@ -45,4 +54,9 @@ func (c *Chip8) ReadInstruction() (Instruction, error) {
 	// automatically increase after every read
 	c.PC = c.PC + 2
 	return instruction, nil
+}
+
+// toString
+func (c Chip8) String() string {
+	return fmt.Sprintf("PC: %d\nInstruction: %d\nRegisters: %v\nStack: %v", c.PC, c.I, c.Registers, c.Stack)
 }
